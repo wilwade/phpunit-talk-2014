75c131ce4e0986d7e67470facb56619a5fa2405b
Wil Wade <wil@supplyhog.com>
2014-09-30 11:56:26 -0400

    Money Example

---
+++ money/src/Money.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* From https://phpunit.de/getting-started.html
+*/
+class Money
+{
+    private $amount;
+
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function negate()
+    {
+        return new Money(-1 * $this->amount);
+    }
+
+    // ...
+}
---
+++ money/src/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('Money.php');
+
