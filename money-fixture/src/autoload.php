dc09246b2f1d55acd70bdc34d3c6cf62d028be33
Wil Wade <wil@supplyhog.com>
2014-09-30 14:47:06 -0400

    Even better autoloader

--- money-fixture/src/autoload.php
+++ money-fixture/src/autoload.php
@@ -1,7 +1,10 @@
 <?php
 
 function autoloader ($className) {
-	include(__DIR__ . "/" . $className . ".php");
+	$file = __DIR__ . "/" . $className . ".php";
+	if(!file_exists($file))
+		return false;
+	include($file);
 }
 spl_autoload_register("autoloader");
 
